ed67b4396435e99e65a9be5e15c7531134ad8df8
alexnebesky <alexnebesky@gmail.com>
2019-04-28 21:14:24 +0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            var str = Console.ReadLine();
+            int num;
+            bool isNum = int.TryParse(str, out num);
+            if (isNum)
+            {
+                Console.WriteLine("Это число");
+                var list = new List<int>();
+                for (var i = 0; i < isNum; i++)
+                {
+                    list.Add(i);
+                }
+
+                for (var i = 0; i < list.Count; i++)
+                {
+                    var fact = (i);
+                }
+
+            }
+
+            else
+                Console.WriteLine("Попробуйте снова:");
+            Console.ReadLine();
+
+        }
+    }
+}
